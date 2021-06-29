d9d11f03a55ea33a43b110eedcfad02b58c0f0a4
ClemColl <yclem.collongues@gmail.com>
2021-06-29 18:22:19 +0200

    updated rankr6 command due to api changes

--- commands/rankr6.js
+++ commands/rankr6.js
@@ -1,7 +1,5 @@
-const R6API = require('r6api.js').default;
 const { r6Mail, r6Pass } = require('../config.js')
-console.log(r6Mail)
-const r6api =  new R6API({ r6Mail, r6Pass });
+const R6API = require('r6api.js').default;
 const pool = require('../clientpool.js')
 
 module.exports = {
@@ -10,18 +8,20 @@ module.exports = {
 	async execute(message, args) {
         console.log(`Executing ${this.name} command, initiated by ${message.author.username}, with args ${args}`);
         
+        
+        const r6api =  new R6API({email: r6Mail, password: r6Pass });
         const user_id = message.author.id
         const queryText = `SELECT platform_id FROM account_table WHERE user_id = '${user_id}' and platform_name = 'uPlay'`;
 
         await pool.query(queryText)
         .then(async (result) => {
                 const platform_id = result.rows[0].platform_id
-                await r6api.getRanks('uplay', platform_id, { regions: ['emea'], boardIds: 'pvp_ranked', seasons: [-1] })
+                await r6api.getRanks('uplay', platform_id, { regionsIds: 'emea', boardIds: 'pvp_ranked', seasons: [-1] })
                 .then(result => {
                         const data = result[0].seasons
-                        const rank = Object.values(data)[0].regions.emea.current
-
-                        message.reply('tu es ' + rank.name + ' avec ' + rank.mmr + ' de MMR.', {files: [rank.image]})
+                        const rank = Object.values(data)[0].regions.emea.boards.pvp_ranked.current
+                        
+                        message.reply('tu es ' + rank.name + ' avec ' + rank.mmr + ' de MMR.', {files: [rank.icon]})
                 }
                 , reason => {
                         message.reply(`Erreur de merde: ${reason})`)
