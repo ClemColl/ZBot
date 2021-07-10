27521391e3452df2245dce553e211df15105c65b
ClemColl <yclem.collongues@gmail.com>
2021-07-10 19:53:36 +0200

    fixed rankapex cmd

--- commands/rankapex.js
+++ commands/rankapex.js
@@ -8,9 +8,15 @@ module.exports = {
 	async execute(message, args) {
         console.log(`Executing ${this.name} command, initiated by ${message.author.username}, with args ${args}`);
         
-        const platform_id = "Twitch_Zepri"
+
+        const user_id = message.author.id
+
+        const queryText = "SELECT platform_id FROM account_table WHERE user_id = '" + user_id + "' and platform_name = 'Origin'";
+        const result = await pool.query(queryText)
+        if (result.rows[0] == undefined) return message.reply("Impossible de te trouver dans la base de données... Vous n'êtes pas assez compétitif?")
+        const platform_id = result.rows[0].platform_id
         
-        const response = await fetch(`https://api.mozambiquehe.re/bridge?version=5&platform=PC&player=${platform_id}&auth=${apex_key}`)
+        const response = await fetch(`https://api.mozambiquehe.re/bridge?version=5&platform=PC&uid=${platform_id}&auth=${apex_key}`)
         const data = await response.json()
         //const user_id = message.author.id
         //const queryText = `SELECT platform_id FROM account_table WHERE user_id = '${user_id}' and platform_name = 'uPlay'`;
